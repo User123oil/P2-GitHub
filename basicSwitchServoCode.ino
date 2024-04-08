71cfed2a5c7fc90ba55101608eb96ab47254cf26
User123oil <165952331+User123oil@users.noreply.github.com>
2024-04-08 13:09:10 -0500

    Update basicSwitchServoCode.ino

--- basicSwitchServoCode.ino
+++ basicSwitchServoCode.ino
@@ -5,6 +5,7 @@ Servo myservo;
 void setup() {
   // put your setup code here, to run once:
   pinMode(switchPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   myservo.attach(10);
   myservo.write(0);
 
@@ -14,8 +15,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(switchPin) == HIGH){
     myservo.write(180);
+    digitalWrite(LED_BUILTIN, HIGH);
   } else {
     myservo.write(0);
+    digitalWrite(LED_BUILTIN, LOW);
   }
 
 }
